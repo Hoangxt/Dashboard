b49f1a3935429cb23f3a547de32a72f87e4bdf7f
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-08-22 22:37:14 +0700

    Done 3 chart

--- src/compontents/Charts/Stacked.jsx
+++ src/compontents/Charts/Stacked.jsx
@@ -19,18 +19,21 @@ import {
 import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
-      width={width}
-      height={height}
-      id="charts"
+      id="stacked-chart"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
       legendSettings={{ background: "white" }}
     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
         {stackedCustomSeries.map((item, index) => (
           <SeriesDirective key={index} {...item} />
